3ab184c63a670065f1eb9299742c5f72de974f27
Yusuf <mikoofficial404@gmail.com>
2025-06-24 21:54:27 +0700

    add user

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,12 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) {
     return $request->user();
-});
+})->middleware('auth:sanctum');
 
 Route::post('login', [AuthController::class, 'login']);
 Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:api');
+Route::get('users', [UserController::class, 'index']);
 
 Route::middleware(['auth:api'])->group(function () {
     Route::middleware(['role:admin'])->group(function () {
